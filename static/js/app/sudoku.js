5d726a8a1eaddf352ddf3559eef91fefc8b163c2
Steve Vadocz <steve.vadocz@guardian.co.uk>
2014-09-25 12:38:47 +0100

    Rendering puzzle.

--- static/js/app/sudoku.js
+++ static/js/app/sudoku.js
@@ -4,9 +4,22 @@ var sudoku = function() {
 		cells : qwery(".sudoku__cell")
  	};
 
+ 	var easyPuzzle = [
+	    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+	    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+	    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+	    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+	    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+	    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+	    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+	    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+	    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+	];
+
  	var api = {
  		init: function() {
  			this.bindEvents();
+ 			this.renderPuzzle();
  		},
 
  		bindEvents: function() {
@@ -15,6 +28,15 @@ var sudoku = function() {
 
  		clickHandler: function(e) {
  			console.log('click', e.target);
+ 		},
+
+ 		renderPuzzle: function() {
+ 			for (var i = 0; i < 81; i++) {
+ 				var row = Math.floor(i / 9),
+ 					col = i % 9;
+
+ 				document.getElementById(i).innerHTML = "<span>" + easyPuzzle[row][col] + "</span>";
+ 			}
  		}
  	};
 
